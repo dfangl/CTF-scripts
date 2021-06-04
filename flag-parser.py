86edc56188ef2a97fe8e60a132c9d6d0e1343ee4
Daniel Fangl <daniel.fangl@gmail.com>
2021-06-04 15:41:39 +0200

    add output parser

---
+++ flag-parser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+from typing import List
+import re
+
+FLAG_REGEX = re.compile(r'\d{14}UTC[A-Z0-9]{15}')
+
+def parse_string(input: str) -> List[str]:
+    return re.findall(FLAG_REGEX, input)
+
+if __name__ == "__main__":
+    input = sys.stdin.read()
+    #print(input)
+    flags = parse_string(input)
+    print("\n".join(flags))
+    #print(f"Found {len(flags)} flags!")
